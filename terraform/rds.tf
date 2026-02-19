9e63cbfb01342419ad2ec74a969abd01e68b9147
Logan McNatt <logan.m.mcnatt@gmail.com>
2026-02-18 20:58:54 -0700

    Make RDS available, hopefully

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,8 +1,8 @@
-# When allowed_mysql_cidr is set, use only public subnets so RDS is placed in one and gets a public endpoint.
-# Otherwise use only private subnets (RDS not reachable from internet).
+# Always include both public and private subnets so that toggling publicly_accessible
+# doesn't try to remove the subnet the running instance sits in.
 resource "aws_db_subnet_group" "main" {
   name       = "${local.name_prefix}-db-subnet"
-  subnet_ids = var.allowed_mysql_cidr != "" ? aws_subnet.public[*].id : aws_subnet.private[*].id
+  subnet_ids = concat(aws_subnet.public[*].id, aws_subnet.private[*].id)
 
   tags = {
     Name = "${local.name_prefix}-db-subnet"
@@ -23,6 +23,7 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   publicly_accessible    = var.allowed_mysql_cidr != ""
+  apply_immediately      = true
   multi_az              = false
 
   allocated_storage     = 20
